5b4ec1da61056920570c352d68067e0c078b2c24
David <fumey.david@hotmail.com>
2018-11-19 16:45:35 +0100

    Gestion de caméra

---
+++ Assets/Scripts/OptionsButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsButton : MonoBehaviour {
+
+    public GameObject optionsMenu;
+    bool checking = false;
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void displayOptions()
+    {
+        
+        if (!checking)
+        {
+            optionsMenu.SetActive(true);
+            checking = true;
+        }
+
+        else
+        {
+            optionsMenu.SetActive(false);
+            checking = false;
+        }
+        
+    }
+}
