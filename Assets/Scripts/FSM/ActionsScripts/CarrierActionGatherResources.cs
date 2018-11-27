ba6e023d4f734aa4f63a825f0e8d51ac3f9f7ceb
David <fumey.david@hotmail.com>
2018-11-27 19:11:41 +0100

    Avancées FSM Carrier

---
+++ Assets/Scripts/FSM/ActionsScripts/CarrierActionGatherResources.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "PluggableAI/Actions/Carrier/Gather Resources")]
+public class CarrierActionGatherResources : FSMAction
+{
+
+    public override void Act(FSMControler controler)
+    {
+        GatherResources(controler);
+    }
+
+    private void GatherResources(FSMControler controler)
+    {
+        //controler.citizen.addRessource(ressource)    Ajouter la ressource dans le sac du carrier
+
+        //foreach(Ressource item in controler.target.GetComponent<Building>().ressourcesNeeded.Values)
+        /*
+        for(int i = 0; i < controler.target.GetComponent<Building>().ressourcesNeeded.Values.Count; i++)
+        {
+            controler.citizen.addRessource(controler.target.GetComponent<Building>().ressourcesNeeded.Keys.)
+        }*/
+    }
+
+}
---
+++ Assets/Scripts/FSM/DecisionsScripts/CarrierDecisionResourcesGathered.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "PluggableAI/Decisions/Carrier/Resources Gathered")]
+public class CarrierDecisionResourcesGathered : FSMDecision
+{
+    public override bool Decide(FSMControler controler)
+    {
+        return ResourcesGathered(controler);
+    }
+
+    private bool ResourcesGathered(FSMControler controler)
+    {
+        Dictionary<Ressource, int> inv = controler.citizen.inventaire;
+        Dictionary<Ressource, int> invBuilding = controler.target.GetComponent<Building>().ressourcesNeeded;
+        return true;
+        // TODO
+        // Comparer les deux dicos entre eux
+    }
+}
