88222ccf0551afb115dc329142d5e9c5768a0d4f
MehdiBeMyGuest <mehdibenlamine@hotmail.fr>
2018-12-04 16:54:30 +0100

    Les builders vont vers automatiquement vers les batiments non construits

---
+++ Assets/Scripts/FSM/ActionsScripts/Builder/BuilderActionFindBuilding.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "PluggableAI/Actions/Builder/Find Building")]
+public class BuilderActionFindBuilding : FSMAction
+{
+
+    public override void Act(FSMControler controler)
+    {
+        FindBuildingNotConstruct(controler);
+    }
+
+    public void FindBuildingNotConstruct(FSMControler controler)
+    {
+        Building[] buildings = FindObjectsOfType<Building>();
+
+        foreach(Building building in buildings)
+        {
+            if (!building.isConstruct)
+            {
+                controler.target = building.gameObject;
+            }
+        }
+
+    }
+}
