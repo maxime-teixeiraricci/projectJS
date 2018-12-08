526695298d36a4e04b56b0e0de7a7e4913be03b6
David <36199829+David-Fumey@users.noreply.github.com>
2018-12-08 18:04:00 +0100

    Slider Environnement

---
+++ Assets/Scripts/Managers/FactorsManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FactorsManager : MonoBehaviour {
+
+    public Slider slider;
+    float res;
+
+    float value;
+    public int nbrTrees;
+
+    float maxRessources;
+
+
+    private void Start()
+    {
+        maxRessources = nbrTrees * 500;
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        res = environmentShape();
+        slider.value = res / maxRessources * 100;
+	}
+
+    public float environmentShape()
+    {
+        res = 0;
+        GameObject[] trees = GameObject.FindGameObjectsWithTag("RessourceTank");
+        foreach(GameObject tree in trees)
+        {
+            value = tree.GetComponent<NaturalRessource>().numberRessource;
+            res += value;
+        }
+
+        return res;
+    }
+}
