1a8d8d6464c73f1d85c7c3755aa3afce8683d861
David <36199829+David-Fumey@users.noreply.github.com>
2018-12-10 13:31:58 +0100

    FeedBack clic tree

--- Assets/Scripts/FocusTrees.cs
+++ Assets/Scripts/FocusTrees.cs
@@ -5,25 +5,39 @@ using UnityEngine;
 public class FocusTrees : MonoBehaviour {
 
     List<Citizen> citizen;
+    public MeshRenderer mesh;
+
+    Color originalColor;
 
 
     // Use this for initialization
     void Start ()
     {
         citizen = GameObject.Find("Dispatcher").GetComponent<DispatcherManager>().collectCitizens;
+        originalColor = mesh.material.color;
     }
 	
 	// Update is called once per frame
 	void Update ()
     {
-
+        
 	}
 
     private void OnMouseDown()
     {
-        foreach(Citizen cit in citizen)
+        
+        foreach (Citizen cit in citizen)
         {
             cit.GetComponent<FSMControler>().manualTarget = gameObject;
         }
+        StartCoroutine(colorChange());
+
+    }
+
+    IEnumerator colorChange()
+    {
+        mesh.material.color = Color.red;
+        yield return new WaitForSeconds(0.5f);
+        mesh.material.color = originalColor;
     }
 }
