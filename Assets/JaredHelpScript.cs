fd45334d0ce3722300bfc08d0e863894bf426a97
Maxime Teixeira <maxime.teixeira.93@gmail.com>
2018-12-16 00:12:17 +0100

    Aide Jared

---
+++ Assets/JaredHelpScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class JaredHelpScript : MonoBehaviour
+{
+    public Camera cam;
+    public Color normal;
+    public Color over;
+    public GameObject text;
+
+    public void Start()
+    {
+        normal = GetComponent<MeshRenderer>().material.color;
+        over = Color.white;
+    }
+
+    public void Update()
+    {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        GetComponent<MeshRenderer>().material.color = normal;
+        text.SetActive(false);
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform.gameObject == gameObject )
+            {
+                GetComponent<MeshRenderer>().material.color = over;
+                text.SetActive(true);
+            }
+        }
+    }
+
+}
