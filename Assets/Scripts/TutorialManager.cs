84df7aa121ea3e8002d17ebdbb8cdd984c038fed
David <36199829+David-Fumey@users.noreply.github.com>
2019-01-06 16:31:10 +0100

    Background interactables disabled

--- Assets/Scripts/TutorialManager.cs
+++ Assets/Scripts/TutorialManager.cs
@@ -1,10 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TutorialManager : MonoBehaviour {
 
     public GameObject nextTuto;
+    public Slider gatherers;
+    public Slider crafters;
+    public Slider carriers;
+    public Slider builders;
+
+    public Button camp;
+    public Button house;
+    public Button statue;
 
     // Use this for initialization
     void Start () {
@@ -13,7 +22,16 @@ public class TutorialManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (gameObject.activeSelf)
+        {
+            gatherers.interactable = false;
+            crafters.interactable = false;
+            carriers.interactable = false;
+            builders.interactable = false;
+            camp.interactable = false;
+            house.interactable = false;
+            statue.interactable = false;
+        }
 	}
 
     public void skipTuto()
@@ -30,6 +48,13 @@ public class TutorialManager : MonoBehaviour {
 
     public void playGame()
     {
+        gatherers.interactable = true;
+        crafters.interactable = true;
+        carriers.interactable = true;
+        builders.interactable = true;
+        camp.interactable = true;
+        house.interactable = true;
+        statue.interactable = true;
         gameObject.SetActive(false);
     }
 }
